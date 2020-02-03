bd5d44dc439ad315ca95a9643e9fdace6d8ed323
lortino03 <lortino.ansalla@yahoo.fr>
2020-02-03 10:56:22 +0100

    initialisation du projet bookstore

---
+++ bookstore/src/main/java/fr/adamaming/formation/bookstore/BookstoreApplication.java
@@ -0,0 +1,13 @@
+package fr.adamaming.formation.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
---
+++ bookstore/src/test/java/fr/adamaming/formation/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.adamaming.formation.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
